89fd2dd2c5bae9af09ff99b3d3e8ecbf4540d21d
stanislavsabev <bezraboten.34@gmail.com>
2023-02-03 19:39:47 +0100

    Improve play_music.

--- music.py
+++ music.py
@@ -2,14 +2,20 @@ import os
 
 song_number = 0
 music_dir = 'songsforai'
-songs = os.listdir(music_dir) 
 
 
 def play_music(query):
     global song_number
+
+    songs = os.listdir(music_dir) 
+
     if 'previous song' in query:
         song_number -= 1
     elif 'next song' in query:
         song_number += 1
+    song_file = os.path.join(music_dir, songs[song_number])
+    if not os.path.isfile(song_file):
+        print(f"Missing song file {song_file}")
+        return
     print(songs[song_number])
-    os.startfile(os.path.join(music_dir, songs[song_number]))
+    os.startfile(song_file)
